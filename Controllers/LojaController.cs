9eff7d7f27885fd2b7f588b38980c716b7ae0a81
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-09-06 15:18:11 -0300

    feat: first commit

---
+++ Controllers/LojaController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Avaliacao3BimLP3.ViewModels;
+
+namespace Avaliacao3BimLP3.Controllers;
+
+public class LojaController : Controller
+{
+  
+    private static List<LojaViewModel> lojas =
+    new List<LojaViewModel> {
+        new LojaViewModel(1, "piso 3", "Tênis Brasil", "Aqui você encontra os tênis", true, "tenis@email.com"),
+        new LojaViewModel(2, "piso 3", "Lembranças Já", "Vem comprar sua lembrança", false, "lemb@email.com"),
+        new LojaViewModel(3, "piso 1", "Sorvetinho Gelado", "Sorvetinho Gelado", false, "sorvet@email.com"),
+    };
+        
+    public IActionResult Index()
+    {
+        return View(lojas);
+    }
+
+    public IActionResult AdminIndex()
+    {
+        return View(lojas);
+    }
+
+    public IActionResult AdminShow(int id)
+    {
+        return View(lojas[ id-1 ]);
+    }
+
+    public IActionResult AdminDelete(int id)
+    {
+        lojas.RemoveAt(id - 1);
+        return View("AdminIndex");
+    }
+}
---
+++ ViewModels/LojaViewModel.cs
@@ -0,0 +1,21 @@
+namespace Avaliacao3BimLP3.ViewModels;
+
+public class LojaViewModel
+{
+    public int Id { get; set; }
+    public string Piso { get; set; }
+    public string Nome { get; set; }   
+    public string Descricao { get; set; }
+    public bool IsLoja { get; set; }
+    public string Email  { get; set; }
+
+    public LojaViewModel(int id, string piso, string nome, string descricao, bool isLoja, string email)
+    {
+        Id = id;
+        Piso = piso;
+        Nome = nome;
+        Descricao = descricao;
+        IsLoja = isLoja;
+        Email = email;
+    }
+}
